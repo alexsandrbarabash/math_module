6ff3924e8fb5a8b2ed4d83b546f9ddf8756be7da
alexsandrbarabash <sbarabas176@gmail.com>
2021-03-09 21:42:50 +0200

    over

---
+++ main.js
@@ -0,0 +1,12 @@
+const min = require("min").min;
+const summ = require("summ").summ;
+const abs = require("abs").abs;
+console.log("\nMin");
+console.log("======================\n");
+min([45, 45, 45, 23]);
+console.log("\nSumm");
+console.log("======================\n");
+summ(10, 25, 35);
+console.log("\nAbs");
+console.log("======================\n");
+abs(-213);
